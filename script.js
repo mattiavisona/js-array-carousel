3b61e9af42b9de5e5d5f0421930ccb121f9c5edc
mattiavisona <mattiav2706@gmail.com>
2023-03-03 12:11:41 +0100

    structure html

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+// NUOVO ESERCIZIO
+// ________________________________________________________________________________
+
+// Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
+
+// Creo un array dove inserire tutte le mie img
+
+const imgElement = ["carousel-img/01.webp", "carousel-img/02.webp", "carousel-img/03.webp", "carousel-img/04.webp", "carousel-img/05.webp"]
+
