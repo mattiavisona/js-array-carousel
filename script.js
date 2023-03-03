9633dda9acf9420c9365ebcbd78d16a13a6336c9
mattiavisona <mattiav2706@gmail.com>
2023-03-03 18:05:54 +0100

    update js variable

--- script.js
+++ script.js
@@ -5,7 +5,31 @@
 
 // Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
 
-// Creo un array dove inserire tutte le mie img
+/* Creo un array dove inserire tutte le mie img
+- Preparo gli elementi da sintetizzare in js
+- Applicare le varie funzionalità con js
+- Al click del pulsante a sinistra
+---> Cambio l'img verso sinistra
+- Al click del pulsante a destra
+---> Cambio l'img verso destra
+*/
 
+
+// C reo un array dove inserire tutte le mie img
 const imgElement = ["carousel-img/01.webp", "carousel-img/02.webp", "carousel-img/03.webp", "carousel-img/04.webp", "carousel-img/05.webp"]
 
+// Preparo gli elementi da sintetizzare in js 
+
+let leftArrowElement = document.getElementById("left-arrow");
+console.log(leftArrowElement);
+
+let rightArrowElement = document.getElementById("right-arrow");
+console.log(rightArrowElement);
+
+let carouselElement = document.getElementById("carousel");
+console.log(carouselElement);              
+
+
+leftArrowElement.addEventListener("click", function () {
+    
+})
